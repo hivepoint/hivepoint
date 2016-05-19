e3e8167c5e24ba17697ea83731391cbe520b5bf4
Preet <preet@hivepoint.com>
2016-05-19 09:45:43 -0700

    added an interactive example

---
+++ public/components/main-app-section.js
@@ -0,0 +1,41 @@
+Polymer({
+  is: "main-app-section",
+  
+  properties: {
+    item: {
+      type: Object,
+      observer: 'refresh'
+    },
+    control: Object
+  },
+  
+  attached: function() {
+    this._attached = true;
+    this.refresh();
+  },
+  
+  refresh: function() {
+    if (this._attached && this.item) {
+      this.customStyle['--section-background'] = this.item.color;
+      this.updateStyles();
+      
+      if (this.control) {
+        this.$.textPanel.style.display = "none";
+        this.$.controlPanel.style.display = "";
+        var parent = Polymer.dom(this.control).parentNode;
+        if (parent) {
+          Polymer.dom(parent).removeChild(this.control);
+        }
+        Polymer.dom(this.$.controlPanel).appendChild(this.control);
+        this.control.style.display = "";
+      }
+      
+      this.async(function() {
+        this.$.content.style.height = this.$.innerContent.offsetHeight + "px";
+        this.async(function() {
+          this.$.content.style.height = "auto";
+        }, 300);
+      }, 150);
+    }
+  }
+});
