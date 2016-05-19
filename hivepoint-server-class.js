b8655bd0992d1da64682e007fecf17080cf8f0d5
Kingston Duffie <kduffie@sevogle.com>
2016-05-18 17:07:33 -0700

    server made configurable

--- hivepoint-server-class.js
+++ hivepoint-server-class.js
@@ -4,16 +4,23 @@ var http = require('http');
 
 function HivepointServer() { }
 
+HivepointServer.prototype.initialize = function(config, callback) {
+  this.config = config;  
+};
+
 HivepointServer.prototype.start = function(callback) {
-  var clientPort = 31112;
+  var maxAge = 86400000;
+  if (this.config.client.maxAge) {
+    maxAge = this.config.client.maxAge;
+  }
   this.clientApp = express();
   this.clientApp.use(express.static(path.join(__dirname, 'public'), {
-    maxAge: 86400000,
+    maxAge: maxAge,
     etag: false
   }));
   this.clientServer = http.createServer(this.clientApp);
-  this.clientServer.listen(clientPort, function(err) {
-    console.log("Listening for client connections on port " + clientPort);
+  this.clientServer.listen(this.config.client.port, function(err) {
+    console.log("Listening for client connections on port " + this.config.client.port);
     setTimeout(callback, 10);
   }.bind(this));
 };
--- hivepoint-server.js
+++ hivepoint-server.js
@@ -1,14 +1,64 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var path = require('path');
+var cliArgs = require("command-line-args");
+
 var Server = require("./hivepoint-server-class");
 
+
 function start() {
-  var server = new Server();
-  server.start(function(err) {
+  /* define the command-line options */
+  var cli = cliArgs([{
+    name: "help",
+    alias: "h",
+    type: Boolean,
+    description: "Print usage instructions"
+  }, {
+      name: "config",
+      alias: "c",
+      type: String,
+      description: "Path to a configuration file (based on config.json)"
+    }]);
+
+  /* parse the supplied command-line values */
+  var options = cli.parse();
+
+  /* generate a usage guide */
+  var usage = cli.getUsage({
+    header: "HivePoint",
+    footer: "For more information, visit http://hivepoint.com"
+  });
+
+  if (options.help) {
+    console.log(usage);
+    process.exit();
+  }
+
+  var configPath = path.join(__dirname, 'config.json');
+  if (options.config) {
+    configPath = options.config;
+  }
+  console.log("Reading configuration from " + configPath);
+  fs.readFile(configPath, 'utf8', function (err, data) {
     if (err) {
-      console.log("Error starting: " + err);
+      console.log(err);
       process.exit();
     }
+    var config = JSON.parse(data);
+    if (options.domain) {
+      config.domain = options.domain;
+    }
+    console.log("HivePoint server initializing");
+    console.log("Configuration", config);
+    var server = new Server();
+    server.initialize(config);
+    server.start(function (err) {
+      if (err) {
+        console.log("Error starting: " + err);
+        process.exit();
+      }
+    });
   });
 }
 
